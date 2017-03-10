9055b7c87594b66674ad540c2d25091b0a412aa4
zhanghao639 <zhanghao639@pingan.com.cn>
2017-03-10 18:48:05 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+;(function(factory){
+if(typeof define == 'function' && define.amd){
+    //seajs or requirejs environment
+    define(['jquery', 'class', 'picker', 'datepicker', 'timepicker'], factory);
+}else if(typeof module === 'object' && typeof module.exports == 'object'){
+    module.exports = factory(
+        require('jquery'),
+        require('class'),
+        require('picker'),
+        require('datepicker'),
+        require('timepicker')
+    );
+}else{
+    factory(window.jQuery, window.jQuery.klass, window.jQuery.picker, window.jQuery.fn.datepicker, window.jQuery.fn.timepicker);
+}
+})(function($, Class, Picker, DatePicker, TimePicker){
+return Class.$factory('datetimepicker', Picker, {
+    initialize: function(options){
+        var self = this;
+
+        self._super(options);
+
+        self.$datepicker = new DatePicker({
+            container: self.$picker,
+            closeAfterSelect: false
+        });
+
+        self.$timepicker = new TimePicker({
+            container: self.$picker
+        });
+
+        self.$timepicker.on('confirm', function(event, time){
+            var date = self.$datepicker.getDate();
+
+            self.trigger('confirm', [date + ' ' + time, date, time]);
+            self.close();
+        });
+
+        self.$picker.addClass('ui3-datetimepicker');
+    },
+
+    destroy: function(){
+        var self = this;
+
+        self.$datepicker.destroy();
+        self.$datepicker = null;
+        self.$timepicker.destroy();
+        self.$timepicker = null;
+        self._super.destroy.call(self);
+    }
+});
+
+});
